bce8855e581049cbfd41f7d68cde3d2c3810fd72
CleisonFreitas <cleison51@hotmail.com>
2025-04-21 14:15:39 -0300

    feat(skills-section) adding skill section

---
+++ src/app/components/skill-section.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { TbBrandLaravel, TbBrandMysql, TbBrandReactNative } from "react-icons/tb";
+import CustomTitle from "../lib/custom-title";
+import { RiNextjsFill, RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
+import { FaFlutter } from "react-icons/fa6";
+import { BiLogoPostgresql, BiLogoTypescript, BiLogoVuejs } from "react-icons/bi";
+import CustomSkillCard, { CustomSkillCardType } from "../lib/custom-skill-card";
+
+const SkillSection = () => {
+    const stack: CustomSkillCardType[] = [
+        {
+            title: 'Laravel',
+            icon: <TbBrandLaravel size={74} />
+        },
+        {
+            title: 'Next Js',
+            icon: <RiNextjsFill size={74} />
+        },
+        {
+            title: 'React',
+            icon: <RiReactjsLine size={74} />
+        },
+        {
+            title: 'Vue Js',
+            icon: <BiLogoVuejs  size={74} />
+        },
+        {
+            title: 'Typescript',
+            icon: <BiLogoTypescript size={74} />
+        },
+        {
+            title: 'Tailwind Css',
+            icon: <RiTailwindCssFill size={74} />
+        },
+        {
+            title: 'React Native',
+            icon: <TbBrandReactNative size={74} />
+        },
+        {
+            title: 'Flutter',
+            icon: <FaFlutter size={74}/>
+        },
+        {
+            title: 'Postgres',
+            icon: <BiLogoPostgresql size={74} />
+        },
+        {
+            title: 'MySQL',
+            icon: <TbBrandMysql size={74} />
+        }
+    ];
+    return (
+        <section
+            className="flex flex-col justify-between items-center w-full h-auto">
+            <CustomTitle title="Stack" />
+            <div className="grid grid-cols-[repeat(auto-fit,_minmax(140px,_1fr))] gap-4 px-4 w-full">
+                {stack.map((item, index) => (
+                    <CustomSkillCard
+                        key={index}
+                        title={item.title}
+                        icon={item.icon}
+                    />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default SkillSection;
