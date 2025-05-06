a2483c3c67386e3028db29a852086671b104d6a4
CleisonFreitas <cleison51@hotmail.com>
2025-05-05 23:14:43 -0300

    feat(router) adding route and not found page

--- src/app/lib/custom-tabs.tsx
+++ src/app/lib/custom-tabs.tsx
@@ -12,7 +12,7 @@ import { LangType } from "../types/lang_type";
 const CustomTabs = ({ t } : LangType) => {
     return (
         <Tabs defaultValue="all" className="w-full gap-16 lg:gap-4">
-            <TabsList className="grid grid-cols-3 lg:grid-cols-4 justify-between gap-2 w-full bg-gray-950 [&>*]:capitalize [&>*]:text-white [&>*]:border [&>*]:border-orange-400 [&>*]:bg-neutral-900 [&>*]:cursor-pointer [&>*]:px-4 [&>*]:py-2 [&>*]:w-[250px]">
+            <TabsList className="grid grid-cols-3 lg:grid-cols-4 justify-between gap-2 w-full bg-gray-950 [&>*]:capitalize [&>*]:text-white [&>*]:border [&>*]:border-orange-400 [&>*]:bg-neutral-900 [&>*]:cursor-pointer [&>*]:px-4 [&>*]:py-2 [&>*]:w-auto md:[&>*]:w-[250px]">
                 <TabsTrigger value="all">{t['portfolio.option.all']}</TabsTrigger>
                 <TabsTrigger value="gallery">{t['portfolio.option.gallery']}</TabsTrigger>
                 <TabsTrigger value="web_app">{t['portfolio.option.web']}</TabsTrigger>
--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -2,7 +2,7 @@ import 'server-only';
 
 const dictionaries: Record<string, () => Promise<Record<string, string>>> = {
   en: () => import('../../public/dictionaries/en.json').then((module) => module.default),
-  pt: () => import('../../public/dictionaries/pt.json').then((module) => module.default),
+  pt_BR: () => import('../../public/dictionaries/pt_BR.json').then((module) => module.default),
 };
 export type Dictionary = Record<string, string>;
 export const getDictionary = async (locale: string): Promise<Dictionary> => {
